c6e6fba1e4f6f291ebf862e638bb808ddfdc84be
Flantoro <steculalviv@gmail.com>
2024-03-01 12:46:48 +0200

    pageObjects added

--- src/test/java/Tests.java
+++ src/test/java/Tests.java
@@ -5,7 +5,6 @@ public class Tests extends pageObjects{
     @Test
     public void FirstTest(){
         openWebsite();
-
+        loginAsStandartUser();
     }
-
 }
--- src/test/java/pageObjects.java
+++ src/test/java/pageObjects.java
@@ -1,4 +1,6 @@
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -7,6 +9,11 @@ import java.time.Duration;
 public class pageObjects{
     WebDriver driver;
     String websiteUrl = "https://www.saucedemo.com/";
+    String standartUsername = "standard_user";
+    String password = "secret_sauce";
+    By usernameInputField = By.cssSelector("[data-test='username']");
+    By passwordInputField = By.cssSelector("[data-test='password']");
+    By loginButton = By.cssSelector("[id='login-button']");
 
     @BeforeEach
     public void start(){
@@ -14,7 +21,20 @@ public class pageObjects{
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
         driver.manage().window().maximize();
     }
+
+    @AfterEach
+    public void finish(){
+        driver.quit();
+    }
     public void openWebsite(){
         driver.get(websiteUrl);
     }
+
+    public void loginAsStandartUser(){
+        driver.findElement(usernameInputField).sendKeys(standartUsername);
+        driver.findElement(passwordInputField).sendKeys(password);
+        driver.findElement(loginButton).click();
+    }
+
+
 }
